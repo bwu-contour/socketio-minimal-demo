db0eb50bba625a80b425e7ae210ec22c87ee8265
bwu-contour <bing.wu@chase.com>
2024-09-27 16:33:53 -0700

    supports https

--- test_websocket.py
+++ test_websocket.py
@@ -3,19 +3,25 @@ import socketio
 
 ENDPOINT = "http://localhost:8080"
 
+@pytest.fixture
+def sio():
+    if "https" in ENDPOINT.lower():
+        sio = socketio.SimpleClient(ssl_verify=False)
+    else:
+        sio = socketio.SimpleClient()
+    sio.connect(ENDPOINT, transports=['websocket'])  # WebSocket server address
+    yield sio
+    sio.disconnect()
+
 @pytest.mark.repeat(10)  # Run the test 10 times
-def test_send_message():
-    with socketio.SimpleClient() as sio:
-        # ... connect to a server and use the client
-        # ... no need to manually disconnect!
-        sio.connect(ENDPOINT, transports=['websocket'])
-        print('my sid is', sio.sid)
-        print('my transport is', sio.transport)
-        for num in range(1000):
-            sio.emit('message', 'ping' + str(num))
-            try:
-                event = sio.receive(timeout=5)
-            except TimeoutError:
-                print('timed out waiting for event')
-            else:
-                print(f'received event: "{event[0]}" with arguments {event[1:]}')
+def test_send_message(sio):
+    print('my sid is', sio.sid)
+    print('my transport is', sio.transport)
+    for num in range(1000):
+        sio.emit('message', 'ping' + str(num))
+        try:
+            event = sio.receive(timeout=5)
+        except TimeoutError:
+            print('timed out waiting for event')
+        else:
+            print(f'received event: "{event[0]}" with arguments {event[1:]}')
